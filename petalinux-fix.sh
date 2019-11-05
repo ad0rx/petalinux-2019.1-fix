6a8dfcfa4c4b4b4a1ec7a2d125ecbacdb5dfbe2f
Whitlock <Bradley.Whitlock@avnet.com>
2019-11-05 09:34:36 -0700

    'working'

--- petalinux-fix.sh
+++ petalinux-fix.sh
@@ -1,16 +1,32 @@
 #/bin/bash
 #
-#
+# Move some post install intercept scripts to a hold directory
+# which effectively disables them from running.
 
 PETALINUX_ROOT=/app/petalinux/2019.1
 
 function fix {
 
     F=$(find ${PETALINUX_ROOT} -name "$1" | grep aarch64)
+    echo "$1: $F"
 
-    echo 'true' >> $F
+    DIR=$(dirname ${F})
+
+    #echo '#!/bin/sh' >  $F
+    #echo 'true'      >> $F
+
+    HOLD=${DIR}/hold
+    if [ ! -e "${HOLD}" ]
+    then
+	echo "Creating Dir: ${HOLD}"
+	mkdir "${HOLD}"
+    fi
+
+    mv "${F}" "${HOLD}"
 
 }
 
 fix "update_icon_cache"
 fix "update_font_cache"
+fix "update_pixbuf_cache"
+fix "update_gio_module_cache"
